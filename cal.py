fce561aed2142bd2fb657baf7211183d948e6317
Steve Webb <bigwebb@gmail.com>
2019-02-28 09:16:06 -0700

    Update cal.py

--- cal.py
+++ cal.py
@@ -4,7 +4,7 @@ with open("in.txt") as fp:
         next(fp) # we don't need the first line, not sure why it's even in there, throw it away
         for line in fp:
                 dow,dom = line.split()
-                row,dnum = divmod(int(dom)+dows.index(dow)-1, 7) # the math that makes it all easy - work smarter, not harder
+                row,dnum = divmod(int(dom)+dows.index(dow)-1, 7) # the math that does it all - work smarter, not harder
                 if (int(dom) > 9):
                         print row, dnum*3, (dnum*3)+1
                 else:
