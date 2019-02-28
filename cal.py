33148c7b354f3a8cfbc96eaf56b7e26612820a85
Steve Webb <bigwebb@gmail.com>
2019-02-28 09:10:27 -0700

    Create cal.py

---
+++ cal.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+dows = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+with open("in.txt") as fp:
+        next(fp) # we don't need the first line
+        for line in fp:
+                dow,dom = line.split()
+                row,dnum = divmod(int(dom)+dows.index(dow)-1, 7)
+                if (int(dom) > 9):
+                        print row, dnum*3, (dnum*3)+1
+                else:
+                        print row, (dnum*3)+1
