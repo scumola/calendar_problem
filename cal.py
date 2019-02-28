0a74420ac5b0524a28d0c10ee746520c92c5932e
Steve Webb <bigwebb@gmail.com>
2019-02-28 09:14:22 -0700

    Update cal.py

--- cal.py
+++ cal.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 dows = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
 with open("in.txt") as fp:
-        next(fp) # we don't need the first line, throw it away
+        next(fp) # we don't need the first line, not sure why it's even in there, throw it away
         for line in fp:
                 dow,dom = line.split()
                 row,dnum = divmod(int(dom)+dows.index(dow)-1, 7)
